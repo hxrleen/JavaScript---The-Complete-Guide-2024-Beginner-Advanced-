2344c0b9a1b1cebac5578f654b6d794fdb146e15
Harleen Raniyal <93335934+hxrleen@users.noreply.github.com>
2024-04-18 12:07:51 +0530

    Add files via upload

--- Personal Movie Database/assets/scripts/app.js
+++ Personal Movie Database/assets/scripts/app.js
@@ -1,160 +1,77 @@
-const addMovieModal = document.getElementById('add-modal');
-// const addMovieModal = document.querySelector('#add-modal');
-// const addMovieModal = document.body.children[1];
-const startAddMovieButton = document.querySelector('header button');
-// const startAddMovieButton = document.querySelector('header').lastElementChild;
-const backdrop = document.getElementById('backdrop');
-// const backdrop = document.body.firstElementChild;
-const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
-const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
-const userInputs = addMovieModal.querySelectorAll('input');
-// const userInputs = addMovieModal.getElementsByTagName('input');
-const entryTextSection = document.getElementById('entry-text');
-const deleteMovieModal = document.getElementById('delete-modal');
+class Product {
+  // title = 'DEFAULT';
+  // imageUrl;
+  // description;
+  // price;
 
-const movies = [];
-
-const toggleBackdrop = () => {
-  backdrop.classList.toggle('visible');
-};
-
-const updateUI = () => {
-  if (movies.length === 0) {
-    entryTextSection.style.display = 'block';
-  } else {
-    entryTextSection.style.display = 'none';
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
   }
-};
+}
 
-const closeMovieDeletionModal = () => {
-  toggleBackdrop();
-  deleteMovieModal.classList.remove('visible');
-};
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
 
-const deleteMovieHandler = movieId => {
-  let movieIndex = 0;
-  for (const movie of movies) {
-    if (movie.id === movieId) {
-      break;
+  addToCart() {
+    console.log('Adding product to cart...');
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      'A Pillow',
+      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+      'A soft pillow!',
+      19.99
+    ),
+    new Product(
+      'A Carpet',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+      'A carpet which you might like - or not.',
+      89.99
+    )
+  ];
+
+  constructor() {}
+
+  render() {
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
     }
-    movieIndex++;
+    renderHook.append(prodList);
   }
-  movies.splice(movieIndex, 1);
-  const listRoot = document.getElementById('movie-list');
-  listRoot.children[movieIndex].remove();
-  // listRoot.removeChild(listRoot.children[movieIndex]);
-  closeMovieDeletionModal();
-  updateUI();
-};
+}
 
-const startDeleteMovieHandler = movieId => {
-  deleteMovieModal.classList.add('visible');
-  toggleBackdrop();
-
-  const cancelDeletionButton = deleteMovieModal.querySelector('.btn--passive');
-  let confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
-
-  confirmDeletionButton.replaceWith(confirmDeletionButton.cloneNode(true));
-
-  confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
-
-  // confirmDeletionButton.removeEventListener('click', deleteMovieHandler.bind(null, movieId)); // will not work :(
-    
-  cancelDeletionButton.removeEventListener('click', closeMovieDeletionModal);
-
-  cancelDeletionButton.addEventListener('click', closeMovieDeletionModal);
-  confirmDeletionButton.addEventListener(
-    'click',
-    deleteMovieHandler.bind(null, movieId)
-  );
-};
-
-const renderNewMovieElement = (id, title, imageUrl, rating) => {
-  const newMovieElement = document.createElement('li');
-  newMovieElement.className = 'movie-element';
-  newMovieElement.innerHTML = `
-    <div class="movie-element__image">
-      <img src="${imageUrl}" alt="${title}">
-    </div>
-    <div class="movie-element__info">
-      <h2>${title}</h2>
-      <p>${rating}/5 stars</p>
-    </div>
-  `;
-  newMovieElement.addEventListener(
-    'click',
-    startDeleteMovieHandler.bind(null, id)
-  );
-  const listRoot = document.getElementById('movie-list');
-  listRoot.append(newMovieElement);
-};
-
-const closeMovieModal = () => {
-  addMovieModal.classList.remove('visible');
-};
-
-const showMovieModal = () => {
-  // function() {}
-  addMovieModal.classList.add('visible');
-  toggleBackdrop();
-};
-
-const clearMovieInput = () => {
-  for (const usrInput of userInputs) {
-    usrInput.value = '';
-  }
-};
-
-const cancelAddMovieHandler = () => {
-  closeMovieModal();
-  toggleBackdrop();
-  clearMovieInput();
-};
-
-const addMovieHandler = () => {
-  const titleValue = userInputs[0].value;
-  const imageUrlValue = userInputs[1].value;
-  const ratingValue = userInputs[2].value;
-
-  if (
-    titleValue.trim() === '' ||
-    imageUrlValue.trim() === '' ||
-    ratingValue.trim() === '' ||
-    +ratingValue < 1 ||
-    +ratingValue > 5
-  ) {
-    alert('Please enter valid values (rating between 1 and 5).');
-    return;
-  }
-
-  const newMovie = {
-    id: Math.random().toString(),
-    title: titleValue,
-    image: imageUrlValue,
-    rating: ratingValue
-  };
-
-  movies.push(newMovie);
-  console.log(movies);
-  closeMovieModal();
-  toggleBackdrop();
-  clearMovieInput();
-  renderNewMovieElement(
-    newMovie.id,
-    newMovie.title,
-    newMovie.image,
-    newMovie.rating
-  );
-  updateUI();
-};
-
-const backdropClickHandler = () => {
-  closeMovieModal();
-  closeMovieDeletionModal();
-  clearMovieInput();
-};
-
-startAddMovieButton.addEventListener('click', showMovieModal);
-backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
-confirmAddMovieButton.addEventListener('click', addMovieHandler);
+const productList = new ProductList();
+productList.render();
