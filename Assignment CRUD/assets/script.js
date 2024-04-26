56209a24207141fe39361dfc4beaa4ff43a59548
Harleen Raniyal <93335934+hxrleen@users.noreply.github.com>
2024-04-26 16:34:37 +0530

    Add files via upload

--- Assignment CRUD/assets/script.js
+++ Assignment CRUD/assets/script.js
@@ -1,21 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     if (checkLoggedIn()) {
         showCRUD();
-        get_json_data(); 
+        get_json_data();
     } else {
         showLoginForm();
     }
 
-    // event listener for login
-    const form = document.getElementById("form");
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        handleLogin();
-    });
 
-    // event listener for logout
+
+    // event listener logout
     const logoutLink = document.querySelector('.logout-link');
-    logoutLink.addEventListener('click', function(event) {
+    logoutLink.addEventListener('click', function (event) {
+
         event.preventDefault();
         handleLogout();
     });
@@ -25,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 // check if any user logged in 
 function checkLoggedIn() {
     const data = JSON.parse(sessionStorage.getItem('sessionData'));
-    return !!data;
+    return data;
 }
 
 // crud table display
@@ -40,19 +36,28 @@ function showLoginForm() {
     document.getElementById("form").style.display = "block";
     document.getElementById("tablecrud").style.display = "none";
     displayGreeting(null);
+    const submitButton = document.getElementById("loginFormSubmit");
+    submitButton.addEventListener("click", handleLoginBtn);
+    function handleLoginBtn() {
+        handleLogin();
+        submitButton.removeEventListener("click", handleLoginBtn);
+    }
+
 }
+
 
 // login
 function handleLogin() {
+
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const jsonData = JSON.parse(localStorage.getItem('jsonData'));
-    const isValidUser = jsonData.some(function(user) {
+    const isValidUser = jsonData.some(function (user) {
         return user.EMAIL === email && user.PASS === password;
     });
 
     if (isValidUser) {
-        const foundUser = jsonData.find(function(user) {
+        const foundUser = jsonData.find(function (user) {
             return user.EMAIL === email;
         });
         const sessionData = {
@@ -108,15 +113,19 @@ function get_json_data() {
         jsonData.forEach((item, index) => {
             const row = document.createElement('tr');
 
-            Object.values(item).forEach(value => {
-                const cell = document.createElement('td');
-                cell.textContent = value;
-                row.appendChild(cell);
+            //order
+            const columns = ['SR', 'UID', 'EMAIL', 'AGE'];
+
+            columns.forEach(columnName => {
+                if (columnName !== 'PASS') {
+                    const cell = document.createElement('td');
+                    cell.textContent = item[columnName];
+                    row.appendChild(cell);
+                }
             });
 
-            // Check if the user's UID matches the logged-in user's UID
             if (item.UID === sessionData.UID) {
-                row.classList.add('highlighted'); // Add the 'highlighted' class to the row
+                row.classList.add('highlighted');
             }
 
             const actionsTd = document.createElement('td');
@@ -146,33 +155,33 @@ function get_json_data() {
 
 
 function showAddModal() {
-var modal = document.getElementById("addModal");
-modal.style.display = "block";
+    var modal = document.getElementById("addModal");
+    modal.style.display = "block";
 }
 
 
 //ADD MODAL show and hide------------------------------------------------------
 
 function hideAddModal() {
-var modal = document.getElementById("addModal");
-modal.style.display = "none";
+    var modal = document.getElementById("addModal");
+    modal.style.display = "none";
 }
 
 var addBtn = document.getElementById("addbtn");
-addBtn.onclick = function() {
-showAddModal();
+addBtn.onclick = function () {
+    showAddModal();
 }
 
 var closeAdd = document.getElementById("closeAdd");
-closeAdd.onclick = function() {
-hideAddModal();
-}
-
-window.onclick = function(event) {
-var modal = document.getElementById("addModal");
-if (event.target == modal) {
+closeAdd.onclick = function () {
     hideAddModal();
 }
+
+window.onclick = function (event) {
+    var modal = document.getElementById("addModal");
+    if (event.target == modal) {
+        hideAddModal();
+    }
 }
 
 //ADD user-------------------------------------------------------------
@@ -181,130 +190,130 @@ var addform = document.getElementById('add-btn');
 addform.addEventListener("click", addUser);
 
 function addUser() {
-const passInput = document.getElementById("password1");
-const emailInput = document.getElementById("email1");
-const ageInput = document.getElementById("age1");
-const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const passInput = document.getElementById("password1");
+    const emailInput = document.getElementById("email1");
+    const ageInput = document.getElementById("age1");
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
-const password = passInput.value.trim();
-const email = emailInput.value.trim();
-const age = ageInput.value.trim();
-const unique = 'BTS_'+ Date.now().toString(16) +Math.floor(Math.random()*1000).toString(16);
-if (email.match(validRegex)) {
-    if (password && email) {
-        if(age){
-            let jsonData = JSON.parse(localStorage.getItem('jsonData')) || [];
+    const password = passInput.value.trim();
+    const email = emailInput.value.trim();
+    const age = ageInput.value.trim();
+    const unique = 'BTS_' + Date.now().toString(16) + Math.floor(Math.random() * 1000).toString(16);
+    if (email.match(validRegex)) {
+        if (password && email) {
+            if (age) {
+                let jsonData = JSON.parse(localStorage.getItem('jsonData')) || [];
 
-            let maxId = 0;
-            jsonData.forEach(user => {
-                if (user.SR > maxId) {
-                    maxId = user.SR;
-                }
-            });
+                let maxId = 0;
+                jsonData.forEach(user => {
+                    if (user.SR > maxId) {
+                        maxId = user.SR;
+                    }
+                });
 
-            const id = parseInt(maxId) + 1;
-
-
-            // Create a new user object
-            const user = {
-                SR: id,
-                UID: unique,
-                EMAIL: email,
-                PASS: password,
-                AGE: age,
-            };
+                const id = parseInt(maxId) + 1;
 
 
-            jsonData.push(user);
+                // Create a new user object
+                const user = {
+                    SR: id,
+                    UID: unique,
+                    EMAIL: email,
+                    PASS: password,
+                    AGE: age,
+                };
 
-            localStorage.setItem("jsonData", JSON.stringify(jsonData));
 
-            passInput.value = "";
-            emailInput.value = "";
-            ageInput.value="";
+                jsonData.push(user);
 
-            get_json_data();
+                localStorage.setItem("jsonData", JSON.stringify(jsonData));
 
-            hideAddModal();
+                passInput.value = "";
+                emailInput.value = "";
+                ageInput.value = "";
+
+                get_json_data();
+
+                hideAddModal();
+            }
+            else {
+                alert("Age is Required");
+            }
+
+        } else {
+            alert("Password is Required");
         }
-        else{
-            alert("Age is Required");
-        }
-
     } else {
-        alert("Password is Required");
+        alert("Invalid email address!");
     }
-} else {
-    alert("Invalid email address!");
-}
 }
 
 
 // UPDATE user --------------------------------------------------------
 function showUpdateForm(userId) {
-let jsonData = JSON.parse(localStorage.getItem('jsonData'));
+    let jsonData = JSON.parse(localStorage.getItem('jsonData'));
 
-const updatePasswordInput = document.getElementById("password2");
-const updateEmailInput = document.getElementById("email2");
-const updateAgeInput = document.getElementById("age2");
+    const updatePasswordInput = document.getElementById("password2");
+    const updateEmailInput = document.getElementById("email2");
+    const updateAgeInput = document.getElementById("age2");
 
-const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 
-const userToUpdate = jsonData.find(user => user.SR === userId);
-console.log(userToUpdate);
-const upmodal = document.getElementById("upModal");
+    const userToUpdate = jsonData.find(user => user.SR === userId);
+    console.log(userToUpdate);
+    const upmodal = document.getElementById("upModal");
 
-updateEmailInput.value = userToUpdate.EMAIL;
-updatePasswordInput.value = userToUpdate.PASS;
-updateAgeInput.value = userToUpdate.AGE;
+    updateEmailInput.value = userToUpdate.EMAIL;
+    updatePasswordInput.value = userToUpdate.PASS;
+    updateAgeInput.value = userToUpdate.AGE;
 
-document.getElementById("upModal").style.display = "block";
-var span = document.getElementById("closeUpdate");
+    document.getElementById("upModal").style.display = "block";
+    var span = document.getElementById("closeUpdate");
 
-span.onclick = function () {
-upmodal.style.display = "none";
-}
+    span.onclick = function () {
+        upmodal.style.display = "none";
+    }
 
-window.onclick = function (event) {
-if (event.target == upmodal) {
-    upmodal.style.display = "none";
-}
-}
-
-var updateBtn = document.getElementById('update-btn');
-
-updateBtn.addEventListener("click", function (event) {
-event.preventDefault();
-const updatedEmail = updateEmailInput.value.trim();
-const updatedPassword = updatePasswordInput.value.trim();
-const updatedAge = updateAgeInput.value.trim();
-
-if (updatedEmail.match(validRegex)) {
-    if (updatedPassword && updatedEmail) {
-        if(updatedAge){
-            userToUpdate.EMAIL = updatedEmail;
-            userToUpdate.PASS = updatedPassword;
-            userToUpdate.AGE = updatedAge;
-
-            localStorage.setItem("jsonData", JSON.stringify(jsonData));
-
-            get_json_data();
-
-            document.getElementById("upModal").style.display = "none";
-        } else {
-            alert("Please fill the age");
+    window.onclick = function (event) {
+        if (event.target == upmodal) {
+            upmodal.style.display = "none";
         }
     }
-    else{
-        alert("Please fill all the fields");
-    }
-}
-else{
-    alert("Please fill valid email.");
-}
-});
+
+    var updateBtn = document.getElementById('update-btn');
+
+    updateBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        const updatedEmail = updateEmailInput.value.trim();
+        const updatedPassword = updatePasswordInput.value.trim();
+        const updatedAge = updateAgeInput.value.trim();
+
+        if (updatedEmail.match(validRegex)) {
+            if (updatedPassword && updatedEmail) {
+                if (updatedAge) {
+                    userToUpdate.EMAIL = updatedEmail;
+                    userToUpdate.PASS = updatedPassword;
+                    userToUpdate.AGE = updatedAge;
+
+                    localStorage.setItem("jsonData", JSON.stringify(jsonData));
+
+                    get_json_data();
+
+                    document.getElementById("upModal").style.display = "none";
+                } else {
+                    alert("Please fill the age");
+                }
+            }
+            else {
+                alert("Please fill all the fields");
+            }
+        }
+        else {
+            alert("Please fill valid email.");
+        }
+    });
 
 }
 
@@ -312,33 +321,33 @@ else{
 //DELETE functions------------------------------------------------------
 
 function deleteUser(userId) {
-let jsonData = JSON.parse(localStorage.getItem('jsonData'));
-let delData = JSON.parse(localStorage.getItem('delData')) || [];
-const sessionData = JSON.parse(sessionStorage.getItem('sessionData'));
+    let jsonData = JSON.parse(localStorage.getItem('jsonData'));
+    let delData = JSON.parse(localStorage.getItem('delData')) || [];
+    const sessionData = JSON.parse(sessionStorage.getItem('sessionData'));
 
-const deletedUser = jsonData.find(user => user.SR === userId);
+    const deletedUser = jsonData.find(user => user.SR === userId);
 
-if (deletedUser.UID === sessionData.UID) {
-    alert("You can not delete yourself");
-    return;
-}
+    if (deletedUser.UID === sessionData.UID) {
+        alert("You can not delete yourself");
+        return;
+    }
 
-jsonData = jsonData.filter(user => user.SR !== userId);
+    jsonData = jsonData.filter(user => user.SR !== userId);
 
-delData.push(deletedUser);
+    delData.push(deletedUser);
 
-localStorage.setItem("jsonData", JSON.stringify(jsonData));
-localStorage.setItem("delData", JSON.stringify(delData));
+    localStorage.setItem("jsonData", JSON.stringify(jsonData));
+    localStorage.setItem("delData", JSON.stringify(delData));
 
-get_json_data(jsonData);
+    get_json_data(jsonData);
 }
 
 
 function confirmDelete(userId) {
-const confirmDelete = confirm("Are you sure you want to delete this user?");
-if (confirmDelete) {
-    deleteUser(userId);
-} else {
-    alert("Delete operation cancelled.");
-}
+    const confirmDelete = confirm("Are you sure you want to delete this user?");
+    if (confirmDelete) {
+        deleteUser(userId);
+    } else {
+        alert("Delete operation cancelled.");
+    }
 }
